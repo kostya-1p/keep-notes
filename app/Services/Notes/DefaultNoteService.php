32e62917a65cba56f4c8dbeb2ffa2e0296b46422
Pavel Kostitsyn <49126820+kostya-1p@users.noreply.github.com>
2023-10-13 14:21:40 +0400

    #2 feat(NoteService): Create function for default note and for todo note
    * #2 feat(): Update readme
    
    * #2 feat(NoteService): Make create function for default note service
    
    * #2 fix(NoteService): return empty array for getContent() if default note text is empty string
    
    * #2 feat(NoteService): Add part of validation to TodoNoteService
    
    * #2 feat(NoteService): Check positions of todo elements
    
    * #2 feat(NoteService): Add check for the first task should not be a subtask

--- app/Services/Notes/DefaultNoteService.php
+++ app/Services/Notes/DefaultNoteService.php
@@ -2,7 +2,26 @@
 
 namespace App\Services\Notes;
 
-class DefaultNoteService
-{
+use App\Data\NoteDataInterface;
+use App\Exceptions\NoteCreateException;
+use App\Repository\NoteRepositoryInterface;
 
+class DefaultNoteService implements NoteServiceInterface
+{
+    public function __construct(protected NoteRepositoryInterface $noteRepository)
+    {
+    }
+
+    public function createNote(NoteDataInterface $noteData): void
+    {
+        if ((is_null($noteData->getTitle()) || $noteData->getTitle() === '') && empty($noteData->getContent())) {
+            throw new NoteCreateException('Empty note discarded');
+        }
+
+        if (!empty($noteData->getContent()) && !is_string($noteData->getContent()[0])) {
+            throw new NoteCreateException('Invalid note content!');
+        }
+
+        $this->noteRepository->store($noteData);
+    }
 }
--- app/Services/Notes/TodoNoteService.php
+++ app/Services/Notes/TodoNoteService.php
@@ -2,7 +2,109 @@
 
 namespace App\Services\Notes;
 
-class TodoNoteService
-{
+use App\Data\NoteDataInterface;
+use App\Data\TodoData;
+use App\Exceptions\NoteCreateException;
+use App\Repository\NoteRepositoryInterface;
 
+class TodoNoteService implements NoteServiceInterface
+{
+    public function __construct(protected NoteRepositoryInterface $noteRepository)
+    {
+    }
+
+    public function createNote(NoteDataInterface $noteData): void
+    {
+        if (!$this->containsTodos($noteData->getContent())) {
+            throw new NoteCreateException('Invalid note content!');
+        }
+
+        if (
+            (is_null($noteData->getTitle()) || $noteData->getTitle() === '')
+            && (empty($noteData->getContent()) || $this->hasEachEmptyText($noteData->getContent()))
+        ) {
+            throw new NoteCreateException('Empty note discarded');
+        }
+
+        if (!$this->isValidTodoPositions($this->getPositionsFrequency($noteData->getContent()))) {
+            throw new NoteCreateException('Note positions is not valid!');
+        }
+
+        if ($this->getFirstPositionTodo($noteData->getContent())?->subtask) {
+            throw new NoteCreateException('First note can not be a subtask!');
+        }
+
+        $this->noteRepository->store($noteData);
+    }
+
+    private function containsTodos(array $todos): bool
+    {
+        foreach ($todos as $todo) {
+            if (!($todo instanceof TodoData)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function hasEachEmptyText(array $todos): bool
+    {
+        $emptyCount = array_reduce($todos, function (int $carry, TodoData $item): int {
+            if ($item->text === '' || is_null($item->text)) {
+                $carry++;
+            }
+            return $carry;
+        }, 0);
+
+        return $emptyCount === count($todos);
+    }
+
+    /** @param TodoData[] $todos */
+    private function getPositionsFrequency(array $todos): array
+    {
+        if (empty($todos)) {
+            return [];
+        }
+
+        $highestPosition = max(array_map(fn(TodoData $todo): int => $todo->position, $todos));
+        $frequency = [];
+        foreach (range(1, $highestPosition) as $position) {
+            $frequency[$position] = 0;
+        }
+
+        foreach ($todos as $todo) {
+            if (!array_key_exists($todo->position, $frequency)) {
+                $frequency[$todo->position] = 1;
+            } else {
+                $frequency[$todo->position]++;
+            }
+        }
+
+        return $frequency;
+    }
+
+    private function isValidTodoPositions(array $frequency): bool
+    {
+        foreach ($frequency as $item) {
+            if ($item !== 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** @param TodoData[] $todos */
+    private function getFirstPositionTodo(array $todos): ?TodoData
+    {
+        if (empty($todos)) {
+            return null;
+        }
+
+        foreach ($todos as $todo) {
+            if ($todo->position === 1) {
+                return $todo;
+            }
+        }
+        throw new NoteCreateException('Invalid note positions!');
+    }
 }
