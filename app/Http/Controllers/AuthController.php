6a968d099783e0d96c814cb45804be2814fb07ae
Pavel Kostitsyn <pavel.kostitsyn@nutnet.ru>
2023-10-16 16:25:01 +0400

    #6 feat(Auth): create login func in controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Data\LoginData;
 use App\Http\Requests\LoginRequest;
+use App\Services\Auth\AuthServiceInterface;
 
 class AuthController extends Controller
 {
+    public function __construct(protected AuthServiceInterface $authService)
+    {
+    }
+
     public function login(LoginRequest $request)
     {
-        return response()->json('yeyash');
+        $loginData = LoginData::from($request->validated());
+        $isLogged = $this->authService->login($loginData);
+        return redirect(route($isLogged ? 'main' : 'login.page'));
     }
 
     public function getLoginPage()
     {
-
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware(['auth', 'auth.session']);
+})->middleware(['auth', 'auth.session'])->name('main');
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/login', 'login')->name('login');
