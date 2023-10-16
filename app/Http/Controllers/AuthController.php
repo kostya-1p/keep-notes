7bedd080ca1a1efd88b8c46dbbcbb9593d993626
Pavel Kostitsyn <pavel.kostitsyn@nutnet.ru>
2023-10-16 14:26:51 +0400

    #6 feat(Auth): create routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+})->middleware(['auth', 'auth.session']);
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login')->name('login');
+    Route::get('/login', 'getLoginPage')->name('login.page');
 });
